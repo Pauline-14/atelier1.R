e9727b6064bb266c3975f26ac44c830c9f902ef7
Pauline-14 <55747551+Pauline-14@users.noreply.github.com>
2019-09-24 15:54:54 +0200

    Add files via upload

---
+++ atelier1.R
@@ -0,0 +1,38 @@
+2+3
+8 - 12
+14 * 25
+-3/10
+2.35/12.2
+1-sqrt(2)
+sort(c(2,5,1,3))
+3*
+5  
+3*
+5  
+2*(3+2)
+x <- 2
+x
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+tailles
+length(tailles)
+mean(tailles) #moyenne
+var(tailles) #variance
+sd(tailles) #écart-type
+summary(tailles) #résumé
+hist(tailles) #il faut plus de valeurs pour faire l'histogramme
+tailles.m <- tailles/100
+tailles.m
+tailles.m = tailles/100
+tailles^2
+sin(tailles)
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+length(poids)
+mean(poids)
+var(poids)
+sd(poids)
+summary(poids)
+imc <- poids/tailles.m^2
+imc #pour afficher les valeurs de imc
+donnees <- data.frame(tailles,poids,imc) #construction d'une base de données
+
+
